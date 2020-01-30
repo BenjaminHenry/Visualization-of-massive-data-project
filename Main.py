17633c0e81fa8442f54ea6bfa0b1daa2f32be5ec
AntoineJahan <antoine.jahan@epitech.eu>
2020-01-30 19:03:08 +0100

    price/availability plot

--- Main.py
+++ Main.py
@@ -14,12 +14,19 @@ from sklearn.cluster import AgglomerativeClustering
 data = pd.read_csv("AB_NYC_2019.csv", index_col=0)
 
 # Selection des 2 colonnes à observer
-my_avail = np.asarray(data['minimum_nights'])
+my_avail = np.asarray(data['availability_365'])
 my_price = np.asarray(data['price'])
+
+# Filtrage des valeurs nulles
+my_avail = list(filter(lambda x:x > 0, my_avail))
+
+# Recoupage de la deuxieme colonne pour avoir la même taille
+my_price = my_price[:len(my_avail)]
+
 my_plot = np.column_stack((my_price, my_avail))
 
 # Selection de valeurs randoms dans les colonnes à observer
-randomRow = np.random.randint(40000, size=50)
+randomRow = np.random.randint(len(my_avail), size=100)
 my_plot = my_plot[randomRow, :]
 
 #print(my_plot)
@@ -28,5 +35,7 @@ cluster = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='w
 cluster.fit_predict(my_plot)
 #print(cluster.labels_)
 plt.scatter(my_plot[:, 0], my_plot[:, 1], c=cluster.labels_, cmap='rainbow')
+plt.xlabel("price")
+plt.ylabel("availability")
 plt.show()
 input()
